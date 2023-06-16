b0d8307850b0206f7011c137daef1ad8a0a7f5de
Alex Sischin <alexsischin@gmail.com>
2023-06-16 18:00:58 +0300

    Made parse_movies a static method

--- IMDBCrawler/spiders/movie_spider.py
+++ IMDBCrawler/spiders/movie_spider.py
@@ -2,41 +2,6 @@ from scrapy import Spider
 from scrapy.http import TextResponse
 
 
-def parse_movies(response: TextResponse):
-    movie_boxes = response.css('.mode-detail')
-    for movie_box in movie_boxes:
-        title = movie_box.css('.lister-item-header a::text').get()
-        year = movie_box.css('span.lister-item-year::text').get()
-        runtime = movie_box.css('span.runtime::text').get()
-        genres = movie_box.css('span.genre::text').get()
-        rating = movie_box.css('.ipl-rating-star__rating::text').get()
-        votes = movie_box.css('span[name="nv"]::text').get()
-
-        if title is not None:
-            title = title.strip()
-
-        if year is not None:
-            year = year.strip()
-
-        if runtime is not None:
-            runtime = runtime.strip()
-
-        if genres is not None:
-            genres = [g.strip() for g in genres.strip().split(r'[,;]')]
-
-        if rating is not None:
-            rating = float(rating.strip())
-
-        yield {
-            'title': title,
-            'year': year,
-            'runtime': runtime,
-            'genres': genres,
-            'rating': rating,
-            'votes': votes,
-        }
-
-
 class MovieSpider(Spider):
     name = 'movies'
     start_urls = [
@@ -44,5 +9,40 @@ class MovieSpider(Spider):
     ]
 
     def parse(self, response: TextResponse, **kwargs):
-        yield from parse_movies(response)
+        yield from self.parse_movies(response)
         yield from response.follow_all(response.css('a.next-page'), self.parse)
+
+    @staticmethod
+    def parse_movies(response: TextResponse):
+        movie_boxes = response.css('.mode-detail')
+        for movie_box in movie_boxes:
+            title = movie_box.css('.lister-item-header a::text').get()
+            year = movie_box.css('span.lister-item-year::text').get()
+            runtime = movie_box.css('span.runtime::text').get()
+            genres = movie_box.css('span.genre::text').get()
+            rating = movie_box.css('.ipl-rating-star__rating::text').get()
+            votes = movie_box.css('span[name="nv"]::text').get()
+
+            if title is not None:
+                title = title.strip()
+
+            if year is not None:
+                year = year.strip()
+
+            if runtime is not None:
+                runtime = runtime.strip()
+
+            if genres is not None:
+                genres = [g.strip() for g in genres.strip().split(r'[,;]')]
+
+            if rating is not None:
+                rating = float(rating.strip())
+
+            yield {
+                'title': title,
+                'year': year,
+                'runtime': runtime,
+                'genres': genres,
+                'rating': rating,
+                'votes': votes,
+            }
